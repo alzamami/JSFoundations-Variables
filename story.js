619768fd2142df9d88f0147b2fbfc3f731e30a62
Mohammad Alzamami <alzamami981@gmail.com>
2021-05-24 16:33:17 +0300

    created a new file

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "Mohammad";
+let age = 22;
+let hobby = "Sports";
+
+console.log(
+  `${name}, in 33 years you will be ${
+    age + 33
+  }. By then you will have decades of ${hobby} practice!`
+);
